e1d6c1470502c47f5617d2a2f703612a507c1a77
Justin Morrison <morrisonxjustin@gmail.com>
2025-09-22 14:43:03 -0400

    Moved main below other functions.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -8,18 +8,18 @@ public class AddressBook
         this.buddyInfoList = new ArrayList<BuddyInfo>();
     }
 
+    public void addBuddy(BuddyInfo buddyInfo){
+        buddyInfoList.add(buddyInfo);
+    }
+    public void removeBuddy(BuddyInfo buddyInfo){
+        buddyInfoList.remove(buddyInfo);
+    }
+
     public static void main(String[] args) {
         System.out.println("AddressBook");
         BuddyInfo buddy = new BuddyInfo("Bob", "Carleton", "1234");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(buddy);
-    }
-
-    public void addBuddy(BuddyInfo buddyInfo){
-        buddyInfoList.add(buddyInfo);
-    }
-    public void removeBuddy(BuddyInfo buddyInfo){
-        buddyInfoList.remove(buddyInfo);
     }
 }
